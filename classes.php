ce51aa29d6b4813185aeaa4a9d006ec737c659fd
bruno <akmacicbruno@gmail.com>
2021-06-26 16:28:35 +0200

    Nova klasa - zapisi

--- classes.php
+++ classes.php
@@ -52,4 +52,29 @@ class Zanr
 		$this->naziv = $naziv;
 	}
 }
+class Zapis 
+{
+	public $zapis_id = "";
+	public $film_id = "";
+	public $naziv = "";
+	public $gledatelj_id = "";
+	public $ime = "";
+	public $prezime = "";
+	public $datum_posudivanja = "";
+	public $rok ="";
+	public $datum_vracanja = "";
+
+	public function __construct ($zapis_id, $film_id, $naziv, $gledatelj_id, $ime, $prezime, $datum_posudivanja, $rok, $datum_vracanja)
+	{
+		$this->zapis_id = $zapis_id;
+		$this->film_id = $film_id;
+		$this->naziv = $naziv;
+		$this->gledatelj_id = $gledatelj_id;
+		$this->ime = $ime;
+		$this->prezime = $prezime;
+		$this->datum_posudivanja = $datum_posudivanja;
+		$this->rok = $rok;
+		$this->datum_vracanja = $datum_vracanja;
+	}
+}
 ?>
