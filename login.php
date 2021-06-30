94cb637a85e7af64dd173145c2b8e2cb89e6022b
bruno <akmacicbruno@gmail.com>
2021-06-30 14:10:23 +0200

    Dodana poveznica za registraciju

--- login.php
+++ login.php
@@ -35,16 +35,20 @@ if(isset($_POST["login"]))
       <head>  
            <title>Videoteka | Prijava</title> 
            <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+           <script src="assets/plugins/angularjs/angular.min.js"></script>
+            <script src="assets/plugins/angularjs/angular-route.min.js"></script>
            <link rel="stylesheet" href="css/login_style.css">
+           <script src="js/registration.js"></script>
+           <link rel="stylesheet" href="css/modal.css">
       </head>  
       <body style="background-image: url(https://s3-us-west-2.amazonaws.com/flx-editorial-wordpress/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg)">  
       <br />  
            <div class="login-container">   
                 <h3 class="login-container__title">Prijava u sustav</h3><br/>  
                 <form method="post" autocomplete="off"> 
-                    <input type="text" name="username" class="login-container__input" autocomplete="off" placeholder="Korisničko ime"/>  
+                    <input type="text" id="username_login" name="username" class="login-container__input" autocomplete="off" placeholder="Korisničko ime"/>  
                     <br/>
-                    <input type="password" name="password" class="login-container__input" autocomplete="off" placeholder="Lozinka"/>  
+                    <input type="password" id="username_pass" name="password" class="login-container__input" autocomplete="off" placeholder="Lozinka"/>  
                     <br/>  
                     <?php  
                         if(isset($message))  
@@ -53,9 +57,10 @@ if(isset($_POST["login"]))
                         }  
                     ?>
                     </br>
-                    <input type="submit" name="login" class="login-container__btn" value="Prijava" />  
+                    <input type="submit" name="login" class="login-container__btn" value="Prijava" /></br></br>
+                    <a type="button" class="login-container__btn" href="registration.php">Registracija</a>  
                 </form>  
            </div>  
            <br />  
       </body>  
- </html> 
+ </html>
